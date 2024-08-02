9ec6284a3c862ee712b0b266cefed50a4d888493
kikealcocerdz <enrique.alcocer1203@gmail.com>
2024-08-02 20:09:14 +0200

    threejs-journey-course

---
+++ 03-first-threejs-project/src/script.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const material = new THREE.MeshBasicMaterial({ color: '#e12220' })
+
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+const sizes = {
+    width: 700,
+    height: 400
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 0
+camera.position.y = 1
+
+
+
+
+scene.add(camera)
+
+const canvas = document.querySelector('canvas.webgl')
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
